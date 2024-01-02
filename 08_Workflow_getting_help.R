081a4b7daed09da5a367240de88383f413680677
danilonastasi <100956845+danilonastasi@users.noreply.github.com>
2024-01-02 21:04:58 +0300

    Create 08_Workflow_getting_help.R

---
+++ 08_Workflow_getting_help.R
@@ -0,0 +1,56 @@
+
+#####  R. version: 4.3.2 2023/10/31  #####
+##### RStudio - 2023.12.0 - Build 369 ######
+
+##### from the book "R for Data Science" #####
+##### Oreilly & Associates Inc; 2nd edition (July 18, 2023) #####
+
+#####    code tested on ______   #####
+
+
+##### 8  Workflow: getting help #####
+
+
+##### 8.1 Google is your friend #####
+
+# (If the error message isn’t in English, run 
+# Sys.setenv(LANGUAGE = "en") 
+# and re-run the code; you’re more likely to find help for English 
+# error messages.)
+
+# If Google doesn’t help, try Stack Overflow. Start by spending a little time 
+# searching for an existing answer, including [R], to restrict your search to 
+# questions and answers that use R.
+
+
+##### 8.2 Making a reprex #####
+
+# If your googling doesn’t find anything useful, it’s a really good idea to 
+# prepare a reprex, short for minimal reproducible example. A good reprex makes it 
+# easier for other people to help you, and often you’ll figure out the problem 
+# yourself in the course of making it. There are two parts to creating a reprex:
+
+  # - First, you need to make your code reproducible. This means that you need to 
+  #   capture everything, i.e. include any library() calls and create all necessary 
+  #   objects. The easiest way to make sure you’ve done this is using the reprex 
+  #   package.
+
+  # - Second, you need to make it minimal. Strip away everything that is not directly 
+  #   related to your problem. This usually involves creating a much smaller and 
+  #   simpler R object than the one you’re facing in real life or even using 
+  #   built-in data.
+
+# That sounds like a lot of work! And it can be, but it has a great payoff:
+
+  # - 80% of the time, creating an excellent reprex reveals the source of your problem. 
+  #   It’s amazing how often the process of writing up a self-contained and minimal 
+  #   example allows you to answer your own question.
+
+  # - The other 20% of the time, you will have captured the essence of your problem 
+  #   in a way that is easy for others to play with. This substantially improves your 
+  #   chances of getting help!
+
+
+
+
+
